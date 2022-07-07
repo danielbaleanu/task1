666ad739262610ba00197b7942408c8d16699967
danielbaleanu <108414000+danielbaleanu@users.noreply.github.com>
2022-07-07 12:48:21 +0300

    Update tproc.c

--- tproc.c
+++ tproc.c
@@ -1,236 +1,227 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <math.h>
+
+FILE *fh1, *fh2;
+
+int i, j, n, lentxt, lentxt1, lentxt2, processing;
+
+double matches, percentage;
+
+char ASCII[256], matchASCII[256], c, repeat = 'y', error;
+
+void var_init(), files(), file2(), file1(), searching(), matching(), progress(),
+		error_check(), print_results(), error_print();
 
 int main() {
-	FILE *fh1, *fh2; // file handling 1, file handling 2
-
-	int i;
-	int j;
-	int m;
-	int read;
-
-	double matches;
-	double percentage;
-	double complete;
-	double lentxt1;
-	double lentxt2;
-	double len2txt1;
-	double k;
-
-	char txt1[1000000];
-	char txt2[1000000];
-	char ASCII[222];
-	char matchtxt2[1000000];
-	char repeat = 'y';
-	char enter;
-	char npc = (' ' - 1); // char out of working range different than null char
-
 	// repeating the main program body until (repeat != 'y')
 	while (repeat == 'y') {
-
-		// non-array variables clearing
-		m = 0;
-		read = 0;
-		matches = 0;
-		percentage = 0;
-		complete = 0;
-		lentxt1 = -1;
-		lentxt2 = 0;
-		len2txt1 = 0;
-		enter = 0;
-
-		// array variables clearing
-		for (i = 0; i < 1000000; i++) {
-			txt1[i] = 0;
-			txt2[i] = 0;
-			matchtxt2[i] = 0;
-		}
-
-		for (i = 0; i < 222; i++) {
-			ASCII[i] = 0;
-		}
-
-		system("clear");
-
-		printf(
-				"Insert/update first and second text in the text files and press return. \n");
-
-		while (enter != '\r' && enter != '\n') {
-			enter = getchar();
-		}
-
-		system("clear");
-		
-		//importing text from text2.txt char by char to txt2 array
-		fh2 = fopen("text2.txt", "r");
-
-		if (fh2 != NULL) {
-			while (!feof(fh2)) {
-				txt2[read] = fgetc(fh2);
-				read++;
-			}
-
-			fclose(fh2);
-		} else {
-			system("clear");
-			printf("Error opening file. \n");
-			return 0;
-		}
-
-		// making a char array containing all unique printable characters plus extended
-		for (i = 0; i < 222; i++) {
-			ASCII[i] = (' ' + i);
-		}
-
-		lentxt2 = strlen(txt2);
-
-		for (j = 0; j < strlen(txt2); j++) {
-			for (i = 0; i < 222; i++) {
-
-				// if a char that is found in ASCII array corresponds to a char found in text1
-				// it will be transfered to matchtxt1 array
-				// the corresponding position of the ASCII array will be substituted with npc char
-				if (ASCII[i] == txt2[j]) {
-					matchtxt2[m] = ASCII[i];
-					ASCII[i] = npc;
-					m++;
-				}
-			}
-
-			// if text1 has more than 10000 chars, percentage count will be initiated
-			if (strlen(txt2) >= 10000) {
-				while (complete <= j + 1) {
-					if (fmod((complete + 1), (lentxt2 / 100)) <= 0.01) {
-						system("clear");
-						printf(
-								"Searching for unique characters in text 2: %lu%% \n",
-								j * 100 / strlen(txt2));
-					}
-					complete += 0.01;
-				}
-			}
-		}
-
-		system("clear");
-
-		printf("Done searching for unique characters in text 2. \n");
-
-		for (k = 0; k < 1000000; k += 0.001) {
-
-		}
-
-		// clearing some of the non-array variables for further use 
-		m = 0;
-		read = 0;
-		complete = 0;
-		lentxt1 = -1;
-		k = 0;
-
-		// matching characters from txt1 with characters from matchtxt2
-		fh1 = fopen("text1.txt", "r");
-
-		if (fh1 != NULL) {
-			while (!feof(fh1)) {
-				txt1[read] = fgetc(fh1);
-				read++;
-			}
-
-			fclose(fh1);
-		} else {
-			system("clear");
-			printf("Error opening file. \n");
-			return 0;
-		}
-
-		// remaking of the char array containing all unique printable characters plus ASCII extended
-		for (i = 0; i < 222; i++) {
-			ASCII[i] = (' ' + i);
-		}
-
-		for (j = 0; j < strlen(txt1); j++) {
-			for (i = 0; i < strlen(matchtxt2); i++) {
-
-				// if a char that is found in matchtxt2 array corresponds to a char found in text1
-				// the number of matches will be incremented
-				if (matchtxt2[i] == txt1[j]) {
-					matches++;
-
-					// if a char that is found in ASCII array corresponds to a char found in text1
-					// that specific char will be removed from ASCII array by substituting it with npc
-					// (non-printable character)
-					if (ASCII[i] == txt1[j] && i < 222) {
-						ASCII[i] = npc;
-					}
-				}
-
-				else if (matchtxt2[i] != txt1[j]) {
-
-					if (ASCII[i] == txt1[j] && i < 222) {
-						ASCII[i] = npc;
-					}
-				}
-			}
-
-			len2txt1 = strlen(txt1); // will be used for float calculations in order to display the progress percentage 
-
-			if (strlen(txt1) >= 10000) // if text1 has more than 10000 chars, percentage count will be initiated
-					{
-				while (complete <= j + 1) {
-					if (fmod((complete + 1), (len2txt1 / 100)) <= 0.01) {
-						system("clear");
-						printf("Checking for matches: %lu%% \n",
-								j * 100 / strlen(txt1));
-					}
-					complete += 0.01;
-				}
-
-				if (txt1[j] != '\n') {
-					lentxt1++;
-				}
-			}
-
-			else if (strlen(txt1) < 10000) {
-				if (txt1[j] != '\n') {
-					lentxt1++;
-				}
-			}
-		}
-
-		system("clear");
-
-		enter = 0;
-
-		printf("Done checking for matches. Press return. \n");
-		while (enter != '\r' && enter != '\n') {
-			enter = getchar();
-		}
-
-		system("clear");
-
-		// displaying the results after both texts have been processed
-		printf("Matches: %d \n", (int) matches);
-		printf(
-				"Number of characters found in text 1 (printable w/ extended): %d \n",
-				(int) lentxt1);
-		percentage = ((matches * 100) / lentxt1);
-		printf(
-				"The percentage of text 1 that can be created using only characters from text 2 is: %f%% \n",
-				percentage);
-		printf("\n");
-		printf("Do you wish to process another set of texts? (y/n) \n");
-		printf("*if so, please enter 'y' for yes. \n");
-		printf(
-				"*otherwise, please insert 'n' for no, in order to terminate the program. \n");
-		printf("\n");
-		printf("Insert response: ");
-		scanf(" %[^\n]%*c", &repeat);
-
+		var_init();
+		files();
+		error_check();
 	}
 
 	system("clear");
-
 	return 0;
+}
+
+// fct for initialisation of array variables 
+void var_init() {
+	system("clear");
+
+	for (i = 0; i < 256; i++) {
+		ASCII[i] = '\0' + i;
+	}
+
+	for (i = 0; i < 256; i++) {
+		matchASCII[i] = '\0';
+	}
+}
+
+void files() {
+	system("clear");
+
+	error = '\0';
+
+	file2();
+	file1();
+}
+
+// fct for counting the number of characters inside of text2.txt & for calling 'searching()' fct
+// executed inside of 'files()'
+void file2() {
+
+	j = 0;
+	n = 0;
+	lentxt2 = 0;
+
+	fh1 = fopen("text1.txt", "r");
+	fh2 = fopen("text2.txt", "r");
+	if (fh1 != NULL && fh2 != NULL) {
+		while (!feof(fh2)) {
+			c = fgetc(fh2);
+			lentxt2++;
+		}
+
+		fclose(fh1);
+		fclose(fh2);
+
+		searching();
+
+	} else {
+		error = 'e';
+	}
+}
+
+// fct that looks for unique characters in text2 and stores them in 'matchASCII' array
+// executed inside of file2()
+void searching() {
+
+	processing = 1;
+	lentxt = lentxt2;
+
+	fh2 = fopen("text2.txt", "r");
+
+	while (!feof(fh2)) {
+		c = fgetc(fh2);
+		for (i = 0; i < 256; i++) {
+			if (ASCII[i] == c && ASCII[i] != '\0') {
+				matchASCII[n] = ASCII[i];
+				ASCII[i] = '\0';
+				n++;
+			}
+		}
+
+		j++;
+
+		if (lentxt >= 10000) {
+			if (j % (lentxt / 100) == 0 && j < lentxt) {
+				system("clear");
+				printf("Processing (%d/2): %d%% \n", processing,
+						j * 100 / lentxt);
+			}
+		}
+	}
+
+	fclose(fh2);
+}
+
+// fct for counting the number of characters inside of text1.txt & for calling 'matching()' fct
+// executed inside of 'files()'
+void file1() {
+
+	j = 0;
+	lentxt1 = -1;
+	matches = -1;
+
+	fh1 = fopen("text1.txt", "r");
+	fh2 = fopen("text2.txt", "r");
+
+	if (fh1 != NULL && fh2 != NULL) {
+		while (!feof(fh1)) {
+			c = fgetc(fh1);
+			lentxt1++;
+		}
+
+		fclose(fh1);
+		fclose(fh2);
+
+		matching();
+
+	} else {
+		error = 'e';
+	}
+}
+
+// fct that looks for matches between the unique chars found previously in text2.txt and chars of text1.txt 
+// executed inside of 'files()' > 'file1()'
+void matching() {
+
+	processing += 1;
+	lentxt = lentxt1;
+
+	fh1 = fopen("text1.txt", "r");
+	fh2 = fopen("text2.txt", "r");
+	while (!feof(fh1)) {
+		c = fgetc(fh1);
+		for (i = 0; i < 256; i++) {
+			if (matchASCII[i] == c && matchASCII[i] != '\0') {
+				matches++;
+			}
+		}
+
+		j++;
+
+		progress();
+	}
+
+	fclose(fh1);
+}
+
+// fct for showing progress during processing
+// executed inside of 'files()' > 'file2()' > 'searching()'
+//								> 'file1()' > 'matching()'
+void progress() {
+	if (lentxt >= 10000) {
+		if (j % (lentxt / 100) == 0 && j < lentxt) {
+			system("clear");
+			printf("Processing (%d/2): %d%% \n", processing, j * 100 / lentxt);
+		}
+	}
+}
+
+// fct for error check
+void error_check() {
+	if (error == '\0') {
+
+		print_results();
+
+	} else if (error == 'e') {
+
+		error_print();
+	}
+}
+
+// fct for displaying results
+void print_results() {
+	system("clear");
+
+	// calculating the percentage of text 1 that can be created using only chars from text 2
+	percentage = (matches * 100) / lentxt1;
+
+	// displaying the results after both texts have been processed
+	printf("Matches: %d \n", (int) matches);
+	printf("Number of characters found in text 1: %d \n", lentxt1);
+
+	if (((int) (percentage * 100)) % 100 == 0) {
+		printf(
+				"The percentage of text 1 that can be created using only characters from text 2 is: %d%% \n",
+				(int) percentage);
+	} else {
+		printf(
+				"The percentage of text 1 that can be created using only characters from text 2 is: %f%% \n",
+				percentage);
+	}
+	printf("\n");
+	printf("Do you wish to process another set of texts? (y/n) \n");
+	printf("*if so, please enter 'y' for yes \n");
+	printf(
+			"*otherwise, please insert 'n' for no, in order to terminate the program \n");
+	printf("\n");
+	printf("Insert response: ");
+	scanf(" %[^\n]%*c", &repeat);
+}
+
+// fct for guiding user in case of error
+void error_print() {
+	system("clear");
+
+	printf("Error opening files. \nDo you wish to try again? (y/n) \n");
+	printf(
+			"*if you have checked the files, please enter 'y', in order to try again \n");
+	printf(
+			"*otherwise, please insert 'n' for no, in order to terminate the program \n");
+	printf("\n");
+	printf("Insert response: ");
+	scanf(" %[^\n]%*c", &repeat);
 }
